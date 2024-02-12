f01fa9373d94c0cd1130e17aaf1c7c631405bf1d
haneen898 <haneenmansoor898@gmail.com>
2024-02-12 12:19:19 +0300

    final

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function showYoungestStudent(students) { }
 
-module.exports = showYoungestStudent;
+students = [
+    {name : "haneen" ,age : 20},
+    { name : "devo", age: 23 },
+     {name: "fefe" , age:19}
+];
+
+
+function youngStudent(students) { 
+    var younges = students[0];
+    for (var i = 0; i < youngStudent.length; i++) {
+        if (students[i].age < younges.age) {younges = students[i]}
+        
+    
+    console.log("the youngest student:" + younges.name)
+    
+}
+}
